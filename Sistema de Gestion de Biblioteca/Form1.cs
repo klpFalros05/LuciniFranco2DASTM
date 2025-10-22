0722a299237a60400dcb2cff76236a9bee648541
klpFalros05 <franlucini05@gmail.com>
2025-10-22 09:41:56 -0300

    Creeacion del tp de entiyy

---
+++ Sistema de Gestion de Biblioteca/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sistema_de_Gestion_de_Biblioteca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
